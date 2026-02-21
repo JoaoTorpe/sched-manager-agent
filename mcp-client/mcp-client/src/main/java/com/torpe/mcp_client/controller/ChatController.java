b7274d8542bf6cc684363bb21dc61ae266231e6a
joaoTorpe <victotorpe10@gmail.com>
2026-02-21 12:38:05 -0300

    Enhance prompt

--- mcp-client/mcp-client/src/main/java/com/torpe/mcp_client/controller/ChatController.java
+++ mcp-client/mcp-client/src/main/java/com/torpe/mcp_client/controller/ChatController.java
@@ -19,20 +19,38 @@ public class ChatController {
     }
 
     @PostMapping
-    public String chat(@RequestBody String question) {
+    public String chat(@RequestBody String input) {
         LocalDate today = LocalDate.now();
 
-        return client.prompt()
-                .user("""
-                        Voce é um agente assistente pessoal, responda a pergunta 
-                        de maneira humana, sucinta e sem mojis.
-                        
-                        Exemplo de resposta: No dia 2 de fevereiro, você tem 3 tarefas: Estudar Spring , Academia (concluída) e Reunião.
-                        
-                        Hoje é %S
-                        Pergunta: %S
-                        """.formatted(today, question))
-                .call()
-                .content();
+        return client.prompt().user("""
+                Você é um agente assistente pessoal
+                
+                        Você receberá um input do usuário e deve responder
+                        de maneira humana, sucinta e sem emojis.
+                
+                
+                        Exemplos de interações:
+                
+                         input: O que tenho para o dia 2 de fevereiro?   
+                         resposta:  Dia 2 de fevereiro, você tem 3 tarefas: Estudar Spring, Academia (concluída) e Reunião de alinhamento com PO.
+                
+                         input: Segunda que vem tenho dentista marcado para às 15 horas, adicione no meu Notion.   
+                         resposta:  'Pronto, Dentista [15:00] adicionado ao calendário' ou 'Certo, Dentista [15:00] adicionado ao calendário'
+                         
+                         input: Amanha preciso comprar ração para o gato.
+                         resposta:  'Certo, Comprar ração do gato adicionado ao calendário'
+                
+                        Informações adicionais:
+                
+                        A atividade é considerada concluída quando o campo 'isDone' é igual a true.
+                
+                        O título das atividades criadas na agenda no Notion deve seguir os seguintes exemplos:
+                            - 'Dentista [15:00]' (Nesse caso o horário foi especificado)
+                            - 'Ortopedista' ou 'Comprar ração do gato' (Nesses casos o horário não foi especificado)
+                
+                
+                Hoje é %S
+                Instrução: %S
+                """.formatted(today, input)).call().content();
     }
 }
